c35c1ce4eb537ca9ce2d1d3db4e5d475d9aaf79e
Gabo <gyturrino@gmail.com>
2025-04-17 00:26:39 -0500

    Correción de configuración de vercel 3

--- api/index.php
+++ api/index.php
@@ -1,14 +1,5 @@
 <?php
-define('LARAVEL_START', microtime(true));
 
-// Rutas ajustadas para la estructura de Vercel
-require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+phpinfo();
 
-// Configuración especial para Vercel
-$app->useStoragePath('/tmp/storage');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle($request = Illuminate\Http\Request::capture());
-$response->send();
-$kernel->terminate($request, $response);
+var_dump(opcache_get_status());
