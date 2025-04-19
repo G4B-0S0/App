fed157ab0387c2bbf0038c2e6d9e5377c975bc75
Gabo <gyturrino@gmail.com>
2025-04-19 03:15:43 -0500

    Cambio estructural y prueba de deploy en vercel prueba 0.1.2

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+define('LARAVEL_START', microtime(true));
+
+// Rutas ajustadas para la estructura de Vercel
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Configuración especial para Vercel
+$app->useStoragePath('/tmp/storage');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle($request = Illuminate\Http\Request::capture());
+$response->send();
+$kernel->terminate($request, $response);
